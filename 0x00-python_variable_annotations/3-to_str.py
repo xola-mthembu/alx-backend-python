44a3d7450f74a3648b08b77e43f66272b5efddca
xola-mthembu <xola.mthembu@gmail.com>
2024-08-01 13:12:45 -0700

    Task 3: to_str function with type annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for to_str function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of the float
+    """
+    return str(n)
