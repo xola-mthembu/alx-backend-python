99e0af9e7935b344977155ba805dc44fda7c444a
xola-mthembu <xola.mthembu@gmail.com>
2024-08-01 13:10:43 -0700

    Task 2: Floor function with type annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module for floor function
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of the float
+    """
+    return math.floor(n)
