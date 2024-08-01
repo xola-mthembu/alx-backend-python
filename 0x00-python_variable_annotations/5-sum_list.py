6a336fd584386a24c7c45c66e83bdbbf4088c63a
xola-mthembu <xola.mthembu@gmail.com>
2024-08-01 14:13:46 -0700

    Task 5: sum_list function with type annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module for sum_list function
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Return the sum of a list of floats
+    """
+    return sum(input_list)
