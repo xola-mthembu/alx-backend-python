549c722496610638698e8ec229e5aef6a31edb40
xola-mthembu <xola.mthembu@gmail.com>
2024-08-01 14:11:07 -0700

    Task 4: Define and annotate variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module to define and annotate variables
+"""
+
+# Define variables with comments indicating types
+a = 1  # type: int
+pi = 3.14  # type: float
+i_understand_annotations = True  # type: bool
+school = "Holberton"  # type: str
