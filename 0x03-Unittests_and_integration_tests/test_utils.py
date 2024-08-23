7c974f4eab57e0ea594a1f8c906f1e0381a8c72a
xola-mthembu <xola.mthembu@gmail.com>
2024-08-23 11:03:28 +0000

    1. Parameterize a unit test for exceptions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Test module for access_nested_map function"""
+"""Test module for access_nested_map exceptions"""
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
@@ -9,13 +9,14 @@ class TestAccessNestedMap(unittest.TestCase):
     """Test case for access_nested_map function"""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b'),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        """Test the access_nested_map function"""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map_exception(self, nested_map, path, err_msg):
+        """Test that KeyError is raised with correct message"""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), f"'{err_msg}'")
 
 
 if __name__ == "__main__":
